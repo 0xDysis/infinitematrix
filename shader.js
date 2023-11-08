7c0b2579209c8e14df188a3d570ed271daee68a8
0xDysis <99392905+0xDysis@users.noreply.github.com>
2023-11-08 16:47:52 +0100

    Add files via upload

--- shader.js
+++ shader.js
@@ -27,11 +27,11 @@ void main() {
     float time = (iTime-2.0)*58.0;
     vec3 col = vec3(0);
     vec3 init = vec3(tan(time * .0032)*.3, .35 - tan(time * .005)*.3, time * 0.0002);
-    for (int r = 0; r < 40; r++) 
+    for (int r = 0; r < 35; r++) 
     {
         vec3 p = init + s * vec3(uv, 0.05);
         p.z = fract(p.z);
-        for (int i=0; i < 10; i++) p = abs(p * 2.04) / dot(p, p) - .9;
+        for (int i=0; i < 9; i++) p = abs(p * 2.04) / dot(p, p) - .9;
         v += pow(dot(p, p), .7) * .06;
         col +=  vec3(v * 0.2+.4, 12.-s*2., .1 + v * 1.) * v * 0.00003;
         s += .025;
